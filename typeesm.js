f13ea5c986312f0383b27a0e53686999850b1a9e
KiiCode Projects <162775830+fahrimuham@users.noreply.github.com>
2024-06-30 13:20:32 +0700

    Create typeesm.js

---
+++ typeesm.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import moment from 'moment-timezone';
+/*====================================================
+Created By KiiCode
+Instagram = @kiicodeit
+Tiktok = @kiicodeproject
+YouTube = @KiiCodeProject
+Channel = https://whatsapp.com/channel/0029VaZSdai5Ui2TMoNsYo0J
+Note: minimal follow sosmed admin dan jangan hapus cr rek
+=====================================================*/
+let handler = async (m, { conn, text }) => {
+    conn.elxyz = conn.elxyz ? conn.elxyz : {};
+
+    if (!text) throw `*• Example:* .elxyz *[on/off]*`;
+
+    if (text == "on") {
+        conn.elxyz[m.chat] = {
+            pesan: []
+        }
+        m.reply("[ ✓ ] Success create session chat")
+    } else if (text == "off") {
+        delete conn.elxyz[m.chat]
+        m.reply("[ ✓ ] Success delete session chat")
+    }
+}
+
+handler.before = async (m, { conn }) => {
+    conn.elxyz = conn.elxyz ? conn.elxyz : {};
+    if (!m.quoted) return
+    const prompt = m.text;
+    let sessions = {};
+    let aiStatus = {};
+    if (!prompt || prompt.startsWith('/'))
+        if (!aiStatus[m.chat]) return;
+
+    console.log(`[${moment().format('HH:mm')}] from @${pushname}: ${prompt}`);
+
+    try {
+        const now = moment().tz('Asia/Jakarta');
+        const jam = now.format('HH:mm');
+        const tgl = now.format('DD-MM-YYYY');
+        const hari = now.format('dddd');
+        let postData = {
+            prompt: prompt,
+            sessionId: `${m.sender}`,
+            character: `Kamu adalah elxyz, kamu adalah ai yang cerdas dan baik`
+        };
+
+        let response = await axios({
+            url: "https://elxyz.me/api/chat",
+            method: 'POST',
+            data: new URLSearchParams(Object.entries(postData)),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        conn.sendMessage(m.chat, { text: `${response.data.data.answer}` }, { mquoted: m });
+    } catch (error) {
+        console.error("Error during chat request:", error);
+        return "An error occurred during the chat process.";
+    }
+}
+
+handler.command = ['elxyz'];
+handler.tags = ["ai"]
+handler.help = ['elxyz'].map(a => a + " *[on/off]*");
+
+export default handler;
